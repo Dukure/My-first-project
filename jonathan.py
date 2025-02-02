7469582e6584c87a285a740d55922a0d6a2696e5
Jonathan Balangaliza Mweze <jonathanmweze@gmail.com>
2025-02-01 22:39:00 -0500

    Commit my project

---
+++ jonathan.py
@@ -0,0 +1,48 @@
+"""x = int(input())
+y = int(input())
+sum = x+y
+print(sum)"""
+"""length = float(input())
+width = float(input())
+
+area = length*width
+print(area)"""
+"""a = 5
+b= a 
+a=3
+print(b)
+x= 10
+y = x+5
+x=20
+print(y)
+a =  int(input())
+b = int(input())
+a,b=swap(a,b)
+print("a =", a)
+print("b=",b)
+a = int(input())
+b = int(input())
+temp = a 
+a = b
+b= temp
+print(a,b)
+course_title = "Python Basics"
+print(course_title)
+a = 9.5
+b = 4.0
+result = a % b
+print(result)
+x = 5
+y = 5
+z = 10
+print(x == y or y > z)
+a = 10
+b = 20
+print(a < b and b > 15)
+x = 5
+y = 5
+z = 10
+print(x == y or y > z)"""
+a = 10
+b = 20
+print(a < b and b > 15)
